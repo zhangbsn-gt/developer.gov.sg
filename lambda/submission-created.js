52954e129a185d988fb65ddb7cc73c2c86fef098
Wei Junyu <50444441+wei-junyu@users.noreply.github.com>
2019-05-16 13:13:54 +0800

    Test printing event in lambda

--- lambda/submission-created.js
+++ lambda/submission-created.js
@@ -2,14 +2,13 @@ const Octokit = require("@octokit/rest");
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 });
-const yaml = require('js-yaml');
+const yaml = require("js-yaml");
 
 exports.handler = async function(event, context, callback) {
     let form = event.payload;
     let site = event.site;
 
-    console.log(JSON.stringify(form, null, 2));
-    console.log(JSON.stringify(site, null, 2));
+    console.log(JSON.stringify(event, null, 2));
 
     let response = await octokit.repos.getContents({
         owner: process.env.GITHUB_OWNER,
@@ -18,7 +17,7 @@ exports.handler = async function(event, context, callback) {
     });
 
     let termsFileYaml = Buffer.from(response.data.content, "base64").toString();
-    
+
     let terms = yaml.safeLoad(termsFileYaml);
 
     console.log(JSON.stringify(terms, null, 2));
