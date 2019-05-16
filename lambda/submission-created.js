18d4152c38bff1d0ad0648df46e1b75456e412f9
Wei Junyu <50444441+wei-junyu@users.noreply.github.com>
2019-05-16 14:53:36 +0800

    Testing get/create ref on github

--- lambda/submission-created.js
+++ lambda/submission-created.js
@@ -3,6 +3,7 @@ const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 });
 const yaml = require("js-yaml");
+const crypto = require("crypto");
 
 exports.handler = async function(event, context, callback) {
     let eventBody = JSON.parse(event.body);
@@ -11,8 +12,10 @@ exports.handler = async function(event, context, callback) {
 
     console.log(JSON.stringify(formData));
 
+    let repoOwner = process.env.GITHUB_OWNER;
+
     let response = await octokit.repos.getContents({
-        owner: process.env.GITHUB_OWNER,
+        owner: repoOwner,
         repo: "developer.gov.sg",
         path: "_data/terms.yml"
     });
@@ -26,19 +29,61 @@ exports.handler = async function(event, context, callback) {
         full_term: formData.full_term,
         description: formData.description,
         link: formData.link,
-        category: formData.category
+        category: [formData.category]
     });
 
     let updatedTermsYaml = yaml.safeDump(terms);
 
     console.log(updatedTermsYaml);
 
-    // Create a new ref (branch)
-    // octokit.git.createRef({owner, repo, ref, sha})
-    // octokit.repos.updateFile({owner, repo, path, message, content, sha})
+    // get ref of latest dev branch
+    const devRef = await octokit.git.getRef({
+        owner: repoOwner,
+        repo: "developer.gov.sg",
+        ref: "heads/dev"
+    });
+
+    console.log(JSON.stringify(devRef));
+    // {
+    //     "ref": "refs/heads/featureA",
+    //     "node_id": "MDM6UmVmcmVmcy9oZWFkcy9mZWF0dXJlQQ==",
+    //     "url": "https://api.github.com/repos/octocat/Hello-World/git/refs/heads/featureA",
+    //     "object": {
+    //       "type": "commit",
+    //       "sha": "aa218f56b14c9653891f9e74264a383fa43fefbd",
+    //       "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/aa218f56b14c9653891f9e74264a383fa43fefbd"
+    //     }
+    //   }
+
+    const newBranchId = await generateId();
+
+    const newRef = await octokit.git.createRef({
+        owner: repoOwner,
+        repo: "developer.gov.sg",
+        ref: "refs/heads/terms-" + newBranchId,
+        sha: devRef.object.sha
+    });
+
+    // get current commit
+    // get current commit's tree
+    // post new blob object with new content => blob SHA
+    // post new tree object with file path pointer replaced with new blob SHA => tree SHA
+    // create new commit with current commit SHA as parent and new tree SHA => commit SHA
+    // update ref to point to commit SHA
 
     callback(null, {
         statusCode: 200,
         body: "success!"
     });
 };
+
+async function generateId() {
+    return new Promise((resolve, reject) => {
+        crypto.randomBytes(12, function(err, buffer) {
+            if (err) {
+                reject(err);
+            }
+            resolve(buffer.toString("hex"));
+        });
+    });
+}
