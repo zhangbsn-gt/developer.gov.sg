8a9d82f6b24375e5179bf52df259a771718304d3
Ronald Kan <ronald_kan@tech.gov.sg>
2019-05-18 14:43:17 +0800

    set terms update to else, if not it will always run

--- lambda/submission-created.js
+++ lambda/submission-created.js
@@ -103,107 +103,108 @@ exports.handler = async function (event, context, callback) {
             body: prResults.data.html_url
         });
         return;
-    }
-
-
-    let response = await octokit.repos.getContents({
-        owner: repoOwner,
-        repo: repoName,
-        path: "_data/terms.yml",
-        ref: ref
-    });
-
-    let termsFileRaw = Buffer.from(response.data.content, "base64").toString();
-
-    let newTerm = [{
-        term: formData.term,
-        full_term: formData.full_term,
-        description: formData.description,
-        link: formData.link,
-        category: [formData.category]
-    }];
-    let newTermYaml = yaml.safeDump(newTerm);
-    let updatedTermsYaml = "";
-    if (termsFileRaw[termsFileRaw.length - 1] === "\n") {
-        updatedTermsYaml = termsFileRaw + newTermYaml;
     } else {
-        updatedTermsYaml = termsFileRaw + "\n" + newTermYaml;
+
+        let response = await octokit.repos.getContents({
+            owner: repoOwner,
+            repo: repoName,
+            path: "_data/terms.yml",
+            ref: ref
+        });
+
+        let termsFileRaw = Buffer.from(response.data.content, "base64").toString();
+
+        let newTerm = [{
+            term: formData.term,
+            full_term: formData.full_term,
+            description: formData.description,
+            link: formData.link,
+            category: [formData.category]
+        }];
+        let newTermYaml = yaml.safeDump(newTerm);
+        let updatedTermsYaml = "";
+        if (termsFileRaw[termsFileRaw.length - 1] === "\n") {
+            updatedTermsYaml = termsFileRaw + newTermYaml;
+        } else {
+            updatedTermsYaml = termsFileRaw + "\n" + newTermYaml;
+        }
+
+        const devRef = await octokit.git.getRef({
+            owner: repoOwner,
+            repo: repoName,
+            ref: `heads/${ref}`
+        });
+
+        const newBranchId = await generateId();
+        const newBranchName = `terms-${new Date().toISOString().substring(0, 10)}-${newBranchId}`;
+        const newRefName = `heads/${newBranchName}`;
+        const newRef = await octokit.git.createRef({
+            owner: repoOwner,
+            repo: repoName,
+            ref: "refs/" + newRefName,
+            sha: devRef.data.object.sha
+        });
+
+        // post new blob object with new content => blob SHA
+        const newBlob = await octokit.git.createBlob({
+            owner: repoOwner,
+            repo: repoName,
+            content: updatedTermsYaml
+        });
+
+        // get current commit => commit object {tree: {url, sha}}
+        const currentCommit = await octokit.git.getCommit({
+            owner: repoOwner,
+            repo: repoName,
+            commit_sha: devRef.data.object.sha
+        });
+
+        // post new tree object with file path pointer replaced with new blob SHA => tree SHA
+        const newTree = await octokit.git.createTree({
+            owner: repoOwner,
+            repo: repoName,
+            tree: [
+                {
+                    path: "_data/terms.yml",
+                    mode: "100644",
+                    type: "blob",
+                    sha: newBlob.data.sha
+                }
+            ],
+            base_tree: currentCommit.data.tree.sha
+        });
+
+        // create new commit with current commit SHA as parent and new tree SHA => commit SHA
+        const newCommit = await octokit.git.createCommit({
+            owner: repoOwner,
+            repo: repoName,
+            message:
+                "new term suggested by " +
+                `${formData.contributor} <${formData.contributor_email}>`,
+            tree: newTree.data.sha,
+            parents: [currentCommit.data.sha]
+        });
+
+        // update ref to point to commit SHA
+        const updateRefResults = await octokit.git.updateRef({
+            owner: repoOwner,
+            repo: repoName,
+            ref: newRefName,
+            sha: newCommit.data.sha
+        });
+
+        // Create new PR to dev
+        const prResults = await octokit.pulls.create({
+            owner: repoOwner,
+            repo: repoName,
+            title: "New Government Term Suggestion",
+            head: newBranchName,
+            base: ref,
+            body: newTermYaml,
+            maintainer_can_modify: true
+        });
+
     }
-
-    const devRef = await octokit.git.getRef({
-        owner: repoOwner,
-        repo: repoName,
-        ref: `heads/${ref}`
-    });
-
-    const newBranchId = await generateId();
-    const newBranchName = `terms-${new Date().toISOString().substring(0, 10)}-${newBranchId}`;
-    const newRefName = `heads/${newBranchName}`;
-    const newRef = await octokit.git.createRef({
-        owner: repoOwner,
-        repo: repoName,
-        ref: "refs/" + newRefName,
-        sha: devRef.data.object.sha
-    });
-
-    // post new blob object with new content => blob SHA
-    const newBlob = await octokit.git.createBlob({
-        owner: repoOwner,
-        repo: repoName,
-        content: updatedTermsYaml
-    });
-
-    // get current commit => commit object {tree: {url, sha}}
-    const currentCommit = await octokit.git.getCommit({
-        owner: repoOwner,
-        repo: repoName,
-        commit_sha: devRef.data.object.sha
-    });
-
-    // post new tree object with file path pointer replaced with new blob SHA => tree SHA
-    const newTree = await octokit.git.createTree({
-        owner: repoOwner,
-        repo: repoName,
-        tree: [
-            {
-                path: "_data/terms.yml",
-                mode: "100644",
-                type: "blob",
-                sha: newBlob.data.sha
-            }
-        ],
-        base_tree: currentCommit.data.tree.sha
-    });
-
-    // create new commit with current commit SHA as parent and new tree SHA => commit SHA
-    const newCommit = await octokit.git.createCommit({
-        owner: repoOwner,
-        repo: repoName,
-        message:
-            "new term suggested by " +
-            `${formData.contributor} <${formData.contributor_email}>`,
-        tree: newTree.data.sha,
-        parents: [currentCommit.data.sha]
-    });
-
-    // update ref to point to commit SHA
-    const updateRefResults = await octokit.git.updateRef({
-        owner: repoOwner,
-        repo: repoName,
-        ref: newRefName,
-        sha: newCommit.data.sha
-    });
-
-    // Create new PR to dev
-    const prResults = await octokit.pulls.create({
-        owner: repoOwner,
-        repo: repoName,
-        title: "New Government Term Suggestion",
-        head: newBranchName,
-        base: ref,
-        body: newTermYaml,
-        maintainer_can_modify: true
-    });
 
     callback(null, {
         statusCode: 200,
