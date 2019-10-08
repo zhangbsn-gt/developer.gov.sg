9dddca3241abb8433eac51c23cab75e5db371d9c
Wei Junyu <weijunyu@users.noreply.github.com>
2019-10-08 14:25:46 +0800

    Moved head include to default layout, added hotjar tracking feature

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -5,6 +5,7 @@ module Jekyll
     def generate(site)
       # Add environment variables to `site.config` here...
       site.config['ga_id'] = ENV['GA_ID'] # Access through site.ga_id
+      site.config['hj_id'] = ENV['HJ_ID'] # Access through site.hj_id
     end
   end
 end
