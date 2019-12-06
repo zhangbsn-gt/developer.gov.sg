61a103306804e2c42df42162aa4320fc179bf88e
Wei Junyu <weijunyu@users.noreply.github.com>
2019-12-06 17:26:45 +0800

    Added email regex tests

--- apps/src/tests/lib.test.js
+++ apps/src/tests/lib.test.js
@@ -1,5 +1,5 @@
 import assert from "assert";
-import { hasErrors } from "../lib";
+import { hasErrors, emailRegex } from "../lib";
 
 describe("lib tests", function() {
   describe("hasErrors", function() {
@@ -12,6 +12,26 @@ describe("lib tests", function() {
     it("returns true for non-empty errors object", function() {
       let hasError = hasErrors({ title: 123, description: null, another: "" });
       assert.strictEqual(hasError, true);
+    });
+  });
+  describe("email regex tests", function() {
+    it("alows abc@def.gov.sg", function() {
+      assert.strictEqual("abc@def.gov.sg".search(emailRegex), 0);
+    });
+    it("allows abc@gov.sg", function() {
+      assert.strictEqual("abc@gov.sg".search(emailRegex), 0);
+    });
+    it("does not allow abc@.gov.sg", function() {
+      assert.strictEqual("abc@.gov.sg".search(emailRegex), -1);
+    });
+    it("does not allow abc@.gov.sg", function() {
+      assert.strictEqual("abc@1gov.sg".search(emailRegex), -1);
+    });
+    it("does not allow abc@def.123.sg", function() {
+      assert.strictEqual("abc@def.123.sg".search(emailRegex), -1);
+    });
+    it("does not allow @gov.sg", function() {
+      assert.strictEqual("@gov.sg".search(emailRegex), -1);
     });
   });
 });
