060f9fd976d67ab8074cfb45c487a046ce964d34
jovanhyw <32458958+jovanhyw@users.noreply.github.com>
2020-02-04 17:41:23 +0800

    Added DSAID blog and refactored blogsapp

--- apps/src/blogs/BlogsApp.vue
+++ apps/src/blogs/BlogsApp.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="blogs-app">
-    <div
-      class="row"
-      v-if="this.gdsBlogs.length===0 && this.datagovBlogs.length===0 && this.ndiBlogs.length===0 && !loading"
-    >
+    <div class="row" v-if="noBlogsFetched && !loading">
       <div class="col">
         <h3
           class="has-text-centered is-size-5"
@@ -13,11 +10,11 @@
     </div>
 
     <div v-else>
-      <div class="row">
+      <div class="row" v-for="agency in allBlogs">
         <div class="col">
-          <h3 class="has-text-weight-bold">Government Digital Services</h3>
+          <h3 class="has-text-weight-bold">{{ agency.name }}</h3>
           <div class="row">
-            <div class="col is-4" v-for="blog in this.gdsBlogs.slice(0, 3)" :key="blog.feedId">
+            <div class="col is-4" v-for="blog in agency.blogEntries.slice(0, 3)" :key="blog.feedId">
               <a :href="blog.url" target="_blank">
                 <div class="sgds-card">
                   <div class="sgds-card-content">
@@ -34,65 +31,7 @@
           </div>
           <p class="has-text-weight-semibold has-text-right is-size-8">
             Read more at the
-            <a
-              href="https://blog.gds-gov.tech/"
-              target="_blank"
-            >Government Digital Services Blog</a>
-          </p>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col">
-          <h3 class="has-text-weight-bold">Data.gov.sg</h3>
-          <div class="row">
-            <div class="col is-4" v-for="blog in this.datagovBlogs.slice(0, 3)" :key="blog.feedId">
-              <a :href="blog.url" target="_blank">
-                <div class="sgds-card sgds-card-button">
-                  <div class="sgds-card-content">
-                    <h5>{{ blog.title }}</h5>
-                    <p class="is-size-8 has-text-weight-light has-text-left">
-                      Published on
-                      {{ blog.pubDate | moment("ddd, D MMM YYYY") }} by
-                      {{ blog.creator }}
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-          <p class="has-text-weight-semibold has-text-right is-size-8">
-            Read more at the
-            <a href="https://blog.data.gov.sg/" target="_blank">Data.gov.sg Blog</a>
-          </p>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col">
-          <h3 class="has-text-weight-bold">National Digital Identity</h3>
-          <div class="row">
-            <div class="col is-4" v-for="blog in this.ndiBlogs.slice(0, 3)" :key="blog.feedId">
-              <a :href="blog.url" target="_blank">
-                <div class="sgds-card sgds-card-button">
-                  <div class="sgds-card-content">
-                    <h5>{{ blog.title }}</h5>
-                    <p class="is-size-8 has-text-weight-light has-text-left">
-                      Published on
-                      {{ blog.pubDate | moment("ddd, D MMM YYYY") }} by
-                      {{ blog.creator }}
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-          <p class="has-text-weight-semibold has-text-right is-size-8">
-            Read more at the
-            <a
-              href="https://medium.com/ndi-sg/"
-              target="_blank"
-            >National Digital Identity Blog</a>
+            <a :href="agency.link.url" target="_blank">{{ agency.link.name }}</a>
           </p>
         </div>
       </div>
@@ -107,28 +46,65 @@ export default {
   data() {
     return {
       loading: false,
-      allBlogs: [],
-      gdsBlogs: [],
-      datagovBlogs: [],
-      ndiBlogs: []
+      allBlogs: [
+        {
+          name: "Government Digital Services",
+          blogEntries: [],
+          link: {
+            url: "https://blog.gds-gov.tech/",
+            name: "Government Digital Services Blog"
+          }
+        },
+        {
+          name: "Data.gov.sg",
+          blogEntries: [],
+          link: {
+            url: "https://blog.data.gov.sg/",
+            name: "Data.gov.sg Blog"
+          }
+        },
+        {
+          name: "National Digital Identity",
+          blogEntries: [],
+          link: {
+            url: "https://medium.com/ndi-sg/",
+            name: "National Digital Identity Blog"
+          }
+        },
+        {
+          name: "Data Science & Artificial Intelligence Division",
+          blogEntries: [],
+          link: {
+            url: "https://medium.com/dsaid-govtech",
+            name: "DSAID Blog"
+          }
+        }
+      ]
     };
-  },
-  methods: {
-    // methods
   },
   created() {
     this.loading = true;
     apiClient
       .get("/blogs")
       .then(response => {
-        this.allBlogs = response.data;
-        this.gdsBlogs = this.allBlogs[0].gdsBlogs;
-        this.datagovBlogs = this.allBlogs[0].datagovBlogs;
-        this.ndiBlogs = this.allBlogs[0].ndiBlogs;
+        this.allBlogs[0].blogEntries = response.data[0].gdsBlogs;
+        this.allBlogs[1].blogEntries = response.data[0].datagovBlogs;
+        this.allBlogs[2].blogEntries = response.data[0].ndiBlogs;
+        this.allBlogs[3].blogEntries = response.data[0].dsaidBlogs;
       })
       .finally(() => {
         this.loading = false;
       });
+  },
+  computed: {
+    noBlogsFetched() {
+      return (
+        this.allBlogs[0].blogEntries.length === 0 &&
+        this.allBlogs[1].blogEntries.length === 0 &&
+        this.allBlogs[2].blogEntries.length === 0 &&
+        this.allBlogs[3].blogEntries.length === 0
+      );
+    }
   }
 };
 </script>
