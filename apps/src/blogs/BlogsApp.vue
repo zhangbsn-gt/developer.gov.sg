b4c90d86037e89d5823c897a70d03ed61fae10b1
Wei Junyu <weijunyu@users.noreply.github.com>
2020-07-07 14:55:50 +0800

    Updated blogs app

--- apps/src/blogs/BlogsApp.vue
+++ apps/src/blogs/BlogsApp.vue
@@ -11,30 +11,31 @@
     <h3
       class="has-text-centered is-size-5"
       style="color: #bdbdbd;"
-      v-else-if="noBlogsFetched && !loading"
+      v-else-if="!loading && blogs.length === 0"
     >
       There are no recent blog posts.
     </h3>
 
-    <div v-else class="row" v-for="agency in allBlogs" :key="agency.name">
+    <div
+      v-else
+      class="row"
+      v-for="feed of blogsGroupedByFeedSortedFiltered"
+      :key="feed.FeedTitle"
+    >
       <div class="col">
         <hr />
-        <h3 class="has-text-weight-bold">{{ agency.name }}</h3>
-        <p v-html="agency.description" />
+        <h3 class="has-text-weight-bold">{{ feed.FeedTitle }}</h3>
+        <p v-html="feed.FeedDescription" />
         <div class="row">
-          <div
-            class="col is-4"
-            v-for="blog in agency.blogEntries.slice(0, 3)"
-            :key="blog.feedId"
-          >
-            <a :href="blog.url" target="_blank">
+          <div class="col is-4" v-for="blog in feed.Blogs" :key="blog.ID">
+            <a :href="blog.Url" target="_blank">
               <div class="sgds-card">
                 <div class="sgds-card-content">
-                  <h5>{{ blog.title }}</h5>
+                  <h5>{{ blog.Title }}</h5>
                   <p class="is-size-8 has-text-weight-light has-text-left">
                     Published on
-                    {{ blog.pubDate | moment("ddd, D MMM YYYY") }} by
-                    {{ blog.creator }}
+                    {{ format(blog.PubDate, "ccc, dd MMM yyyy") }} by
+                    {{ blog.Creator }}
                   </p>
                 </div>
               </div>
@@ -42,8 +43,8 @@
           </div>
         </div>
         <p class="has-text-weight-semibold is-size-8">
-          Read more at the
-          <a :href="agency.link.url" target="_blank">{{ agency.link.name }}</a>
+          Read more at
+          <a :href="feed.FeedUrl" target="_blank">{{ feed.FeedTitle }}</a>
         </p>
       </div>
     </div>
@@ -51,6 +52,7 @@
 </template>
 
 <script>
+import { compareDesc, parseISO, format } from "date-fns";
 import { apiClient } from "../lib";
 
 export default {
@@ -61,7 +63,7 @@ export default {
         {
           name: "Cyber Security Group",
           description: `<strong>Cyber Security Group (CSG)</strong> is the cybersecurity arm of GovTech
-  and is committed to creating a safe and secure digital government.`,
+            and is committed to creating a safe and secure digital government.`,
           blogEntries: [],
           link: {
             url: "https://medium.com/csg-govtech",
@@ -71,8 +73,8 @@ export default {
         {
           name: "Data.gov.sg",
           description: `<strong>Data.gov.sg</strong> is a one-stop portal for access to the
-  government’s publicly available data. It aims to make government data relevant
-  and understandable using charts and articles.`,
+            government’s publicly available data. It aims to make government data relevant
+            and understandable using charts and articles.`,
           blogEntries: [],
           link: {
             url: "https://blog.data.gov.sg/",
@@ -82,9 +84,9 @@ export default {
         {
           name: "Data Science & Artificial Intelligence Division",
           description: `The
-  <strong>Data Science and Artificial Intelligence Division (DSAID) </strong
-  >works with public agencies in using data science and AI to improve service
-  delivery and policy outcomes.`,
+            <strong>Data Science and Artificial Intelligence Division (DSAID) </strong
+            >works with public agencies in using data science and AI to improve service
+            delivery and policy outcomes.`,
           blogEntries: [],
           link: {
             url: "https://medium.com/dsaid-govtech",
@@ -94,8 +96,8 @@ export default {
         {
           name: "Government Digital Services",
           description: `The <strong>Government Digital Service (GDS)</strong> team delivers high-quality
-  digital services to citizens and businesses in Singapore. They have developed
-  products such as Moments-of-Life and Business Grants Portal to name a few.`,
+            digital services to citizens and businesses in Singapore. They have developed
+            products such as Moments-of-Life and Business Grants Portal to name a few.`,
           blogEntries: [],
           link: {
             url: "https://blog.gds-gov.tech/",
@@ -105,8 +107,8 @@ export default {
         {
           name: "National Digital Identity",
           description: `The <strong>National Digital Identity (NDI)</strong> team develops and
-  integrates solutions such as SingPass and MyInfo into a single platform. NDI is
-  also a trusted digital platform for authentication, authorisation and consent.`,
+            integrates solutions such as SingPass and MyInfo into a single platform. NDI is
+            also a trusted digital platform for authentication, authorisation and consent.`,
           blogEntries: [],
           link: {
             url: "https://medium.com/ndi-sg/",
@@ -114,6 +116,7 @@ export default {
           },
         },
       ],
+      blogs: [],
     };
   },
   created() {
@@ -121,25 +124,51 @@ export default {
     apiClient
       .get("/blogs")
       .then((response) => {
-        this.allBlogs[0].blogEntries = response.data.csgBlogs;
-        this.allBlogs[1].blogEntries = response.data.datagovBlogs;
-        this.allBlogs[2].blogEntries = response.data.dsaidBlogs;
-        this.allBlogs[3].blogEntries = response.data.gdsBlogs;
-        this.allBlogs[4].blogEntries = response.data.ndiBlogs;
+        this.blogs = response.data;
       })
       .finally(() => {
         this.loading = false;
       });
   },
   computed: {
-    noBlogsFetched() {
-      return (
-        this.allBlogs[0].blogEntries.length === 0 &&
-        this.allBlogs[1].blogEntries.length === 0 &&
-        this.allBlogs[2].blogEntries.length === 0 &&
-        this.allBlogs[3].blogEntries.length === 0
-      );
+    blogsGroupedByFeed() {
+      return this.blogs.reduce((groupedBlogs, blog) => {
+        blog = {
+          ...blog,
+          PubDate: parseISO(blog.PubDate),
+        };
+        if (groupedBlogs[blog.FeedTitle]) {
+          groupedBlogs[blog.FeedTitle].Blogs = groupedBlogs[
+            blog.FeedTitle
+          ].Blogs.concat([blog]);
+          return groupedBlogs;
+        }
+        return {
+          ...groupedBlogs,
+          [blog.FeedTitle]: {
+            FeedTitle: blog.FeedTitle,
+            FeedUrl: blog.FeedUrl,
+            FeedDescription: blog.FeedDescription,
+            Blogs: [blog],
+          },
+        };
+      }, {});
     },
+    blogsGroupedByFeedSortedFiltered() {
+      return Object.keys(this.blogsGroupedByFeed)
+        .sort((a, b) => a.localeCompare(b))
+        .map((key) => {
+          return {
+            ...this.blogsGroupedByFeed[key],
+            Blogs: this.blogsGroupedByFeed[key].Blogs.sort((a, b) =>
+              compareDesc(a.PubDate, b.PubDate)
+            ).slice(0, 3),
+          };
+        });
+    },
+  },
+  methods: {
+    format,
   },
 };
 </script>
--- apps/src/blogs/vue-blogs-app.js
+++ apps/src/blogs/vue-blogs-app.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import BlogsApp from "./BlogsApp.vue";
 
-Vue.use(require("vue-moment"));
-
 new Vue({
   render: createElement => {
     return createElement(BlogsApp);
