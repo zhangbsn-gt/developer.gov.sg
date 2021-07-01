8b285322080f86f1ced794c02037efe5d9391c36
Tan Kun Sheng <tan_kun_sheng@tech.gov.sg>
2021-07-01 18:22:26 +0800

    Update social media buttons to float with fixed position. Use SGDS icons

--- apps/src/main.js
+++ apps/src/main.js
@@ -1,8 +1,8 @@
 import "./main.scss";
 import "sgds-govtech/js/sgds.js";
 
-$(function() {
-  $(".mobile-sidenav-toggle").click(function() {
+$(function () {
+  $(".mobile-sidenav-toggle").click(function () {
     $(this)
       .children(".sgds-icon")
       .toggleClass("sgds-icon-chevron-down")
@@ -15,4 +15,20 @@ $(function() {
       .next()
       .toggleClass("is-hidden-touch");
   });
+
+  let floaterContainer = $("#floater-container");
+  let floater = $("#floater");
+  let offsetTop = floaterContainer.offset().top;
+  window.onscroll = function () {
+    // console.log(floaterContainer.offset());
+    floater.addClass("float-buttons");
+    // console.log(`innerheight ${window.innerHeight}`);
+    // console.log(`page y offset ${window.pageYOffset}`);
+    
+    if (window.pageYOffset < offsetTop) {
+      console.log("here");
+      floater.removeClass("float-buttons");
+    }
+  };
 });
+
