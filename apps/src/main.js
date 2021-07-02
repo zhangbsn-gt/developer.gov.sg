45b837ccdec64e21d17ef07ca585d05b49ba9470
Tan Kun Sheng <tan_kun_sheng@tech.gov.sg>
2021-07-02 12:12:24 +0800

    Move social media button scripts into module

--- apps/src/main.js
+++ apps/src/main.js
@@ -1,5 +1,6 @@
 import "./main.scss";
 import "sgds-govtech/js/sgds.js";
+import { setupFloatingSocialMediaIcons } from "./lib/socialMediaIcons.js";
 
 $(function () {
   $(".mobile-sidenav-toggle").click(function () {
@@ -10,22 +11,35 @@ $(function () {
     $(".mobile-sidenav-menu").toggleClass("is-hidden");
   });
 
-  $(".sgds-navbar-link.mobile").click(function() {
+  $(".sgds-navbar-link.mobile").click(function () {
     $(this)
       .next()
       .toggleClass("is-hidden-touch");
   });
 
-  let floaterContainer = $("#floater-container");
-  let floater = $("#floater");
-  let offsetTop = floaterContainer.offset().top;
-  window.onscroll = function () {
-    floater.addClass("floating-social-media-buttons");
-    // console.log(`window pageyoffset = ${window.pageYOffset}`);
-    // console.log(offsetTop);
-    if (window.pageYOffset < offsetTop) {
-      floater.removeClass("floating-social-media-buttons");
-    }
-  };
+  // (function () {
+  //   let floaterContainer = $("#floater-container");
+  //   let floater = $("#floater");
+  //   let offsetTop = floaterContainer.offset().top;
+  //   window.onscroll = function () {
+  //     floater.addClass("floating-social-media-buttons");
+  //     // console.log(`window pageyoffset = ${window.pageYOffset}`);
+  //     // console.log(offsetTop);
+  //     if (window.pageYOffset < offsetTop) {
+  //       floater.removeClass("floating-social-media-buttons");
+  //     }
+  //   };
+  //   document.getElementById("#copy-link").addEventListener("click", function () {
+  //     var inputBox = document.getElementById("page-url");
+  //     if (inputBox.classList.contains("hide")) {
+  //       inputBox.classList.remove("hide");
+  //     }
+  //     inputBox.select();
+  //     document.execCommand("copy");
+  //     inputBox.classList.add("hide");
+  //   });
+  // })();
+  setupFloatingSocialMediaIcons();
+
 });
 
