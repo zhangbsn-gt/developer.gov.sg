511dac5f6ade1b6f0fc75c6a89f83c2bc15fbcd5
Tan Kun Sheng <tan_kun_sheng@tech.gov.sg>
2021-07-08 15:12:12 +0800

    Move social media button copy link scripts from main.js to script tag in layout-page-sidenav html script tag. Add checks to copy link element before adding event listener

--- apps/src/main.js
+++ apps/src/main.js
@@ -16,19 +16,6 @@ $(function () {
       .toggleClass("is-hidden-touch");
   });
 
-  (function () {
-    let inputBox = document.getElementById("page-url");
-    let inputBoxMobile = document.getElementById("page-url-mobile");
-    document.getElementById("copy-link").addEventListener("click", function () { copyInputToClipboard(inputBox); });
-    document.getElementById("copy-link-mobile").addEventListener("click", function () { copyInputToClipboard(inputBoxMobile); });
-    function copyInputToClipboard(inputBox) {
-      if (inputBox.classList.contains("hide")) {
-        inputBox.classList.remove("hide");
-      }
-      inputBox.select();
-      document.execCommand("copy");
-      inputBox.classList.add("hide");
-    }
-  })();
+  
 });
 
