e360982f3a1124fc2b250f53d0ac1133c8ebd75c
Teo Cheng Yong <teochengyong@gmail.com>
2021-07-12 09:34:12 +0800

    Using GA to send feedback

--- apps/src/main.js
+++ apps/src/main.js
@@ -1,5 +1,6 @@
 import "./main.scss";
 import "sgds-govtech/js/sgds.js";
+import config from "./lib/config";
 
 $(function() {
   $(".mobile-sidenav-toggle").click(function() {
@@ -14,5 +15,34 @@ $(function() {
     $(this)
       .next()
       .toggleClass("is-hidden-touch");
+  });
+
+  // GA-12345 -> GA_12345 for consistency with GAID set by GA on startup
+  const gaId= config.gaId.replace(/-/g, '_');
+  const eventCategory = 'Test Feedback';
+
+  // Enable feedback feature
+  if (localStorage.getItem('enableFeedback') === '1' ) {
+    $('#feedback').removeClass('is-hidden');
+  }
+
+  $(".js-thumbs-up").click(function() {
+    ga(`gtag_${gaId}.send`, {
+      hitType: 'event',
+      eventCategory: eventCategory,
+      eventAction: 'thumbs-up',
+      eventLabel: 'thumbs-up-icon',
+      eventValue: 1
+    });
+  });
+
+  $("js-thumbs-down").click(function() {
+    ga(`gtag_${gaId}.send`, {
+      hitType: 'event',
+      eventCategory: eventCategory,
+      eventAction: 'thumbs-down',
+      eventLabel: 'thumbs-down-icon',
+      eventValue: 0
+    });
   });
 });
